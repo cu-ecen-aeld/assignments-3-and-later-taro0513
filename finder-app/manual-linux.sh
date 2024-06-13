af7345e435058d1c4da80f184428b04ba58f39bf
taro0513 <hank93513@gmail.com>
2024-06-13 18:59:42 +0800

    fix: bug

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,11 +7,12 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v6.5.13
+KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+PARALLEL_JOBS=-j$(nproc)
 
 if [ $# -lt 1 ]
 then
@@ -34,16 +35,24 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
+    # Apply patch to scripts/dtc/dtc-lexer.l
+    git restore './scripts/dtc/dtc-lexer.l'
+    sed -i '41d' './scripts/dtc/dtc-lexer.l'
+
     # TODO: Add your kernel build steps here
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} $PARALLEL_JOBS mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} $PARALLEL_JOBS defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} $PARALLEL_JOBS all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} $PARALLEL_JOBS modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} $PARALLEL_JOBS dtbs
+else
+    echo "Kernel already built"
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+if [ ! -e ${OUTDIR}/Image ]; then
+    cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
+fi
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -54,53 +63,78 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir -p ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr/bin,usr/lib,usr/sbin,var/log}
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-    git clone git://busybox.net/busybox.git
+git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "Add library dependencies to rootfs"
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
-cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
-cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
-cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+INTERPRETER=$(find $SYSROOT -name "ld-linux-aarch64.so.1")
+cp ${INTERPRETER} ${OUTDIR}/rootfs/lib
+SHARED_LIB_1=$(find $SYSROOT -name "libm.so.6")
+cp ${SHARED_LIB_1} ${OUTDIR}/rootfs/lib64
+SHARED_LIB_2=$(find $SYSROOT -name "libresolv.so.2")
+cp ${SHARED_LIB_2} ${OUTDIR}/rootfs/lib64
+SHARED_LIB_3=$(find $SYSROOT -name "libc.so.6")
+cp ${SHARED_LIB_3} ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+echo "Make device nodes"
 cd ${OUTDIR}/rootfs
 sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 666 dev/console c 5 1
+sudo mknod -m 666 dev/tty c 5 1
 
 # TODO: Clean and build the writer utility
+echo "Clean and build the writer utility"
 cd ${FINDER_APP_DIR}
-make CROSS_COMPILE=${CROSS_COMPILE} clean
-make CROSS_COMPILE=${CROSS_COMPILE}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-cp -aH ${FINDER_APP_DIR}/{autorun-qemu.sh,conf,finder-test.sh,finder.sh,writer,writer.sh} ${OUTDIR}/rootfs/home/
+echo "Copy the finder related scripts and executables to the /home directory"
+cp -f writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+mkdir ${OUTDIR}/rootfs/home/conf
+cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt ${OUTDIR}/rootfs/home/conf
 
 # TODO: Chown the root directory
-sudo chown -R root:root ${OUTDIR}/rootfs
+echo "Chown the root directory"
+cd ${OUTDIR}
+sudo chown -R root:root rootfs
 
 # TODO: Create initramfs.cpio.gz
+echo "Create initramfs.cpio.gz"
 cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-gzip -f ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
